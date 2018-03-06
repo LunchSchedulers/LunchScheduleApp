f3f4aebc74795e9deb85f0d8c03913f260a9cd24
GRADY KNIGHT <gradyknight@GRADYs-MacBook-Pro.local>
2018-03-05 19:55:32 -0500

    initialLogic

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+// Global Variables
+var arrayOfAllEvents = [];
+var exampleEvent = {
+    latitudeOfEvent = "lat",
+    longitudeOfEvent = "long",
+    addressOfEvent = "address",
+    nameOfEventLocation = "name",
+    creatorUserKey = "long user key as a string",
+    eventStartTime = 1234, // unix time
+    eventEndTime = 4321, // unix time
+    eventTypeEnum = 1 // not used for now
+}
+var database;
+
+
+// Database Helper Functions
+function pushUserKeyToLocalStorage(){
+}
+
+function testIfEventIsInProximityWindow(currentLocation, eventLocation, proximityWindowInMiles){
+    return true;
+}
+
+function returnArrayOfAllEventsWithinProximityWindow(currentLocation, proximityWindowInMiles){
+    return arrayOfProximateEvents;
+}
+
+function populateTableWithEvents(){
+    var arrayOfProximateEvents = returnArrayOfAllEventsWithinProximityWindow(currentLocation, proximityWindowInMiles);
+    // jquery stuff to push events into repeating element
+}
+
+function populateFireBaseWithEventInfo(){
+    // jquery to populate info for event
+    // get all exampleEvent element from HTML
+    // push exampleEventObject to firebase
+}
+
+function connectToFirebase(){
+    var config = {
+        apiKey: "AIzaSyAboCNAM8LyGsG2dV-fzJBiZu4UVQhxgHk",
+        authDomain: "meetupschedule-ab2c2.firebaseapp.com",
+        databaseURL: "https://meetupschedule-ab2c2.firebaseio.com",
+        projectId: "meetupschedule-ab2c2",
+        storageBucket: "",
+        messagingSenderId: "799738528055"
+    };
+    firebase.initializeApp(config);
+    database = firebase.database();
+
+}
