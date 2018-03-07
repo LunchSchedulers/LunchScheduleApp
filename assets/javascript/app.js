d3bc15255cccecfc46635fea915f3cc48c171140
Ashley Moore <mooreashleyplease@gmail.com>
2018-03-06 20:24:59 -0500

    greeter form complete, about page skeleton complete, css still needed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,61 @@
-// Global Variables
-var arrayOfAllEvents = [];
-var exampleEvent = {
-    latitudeOfEvent = "lat",
-    longitudeOfEvent = "long",
-    addressOfEvent = "address",
-    nameOfEventLocation = "name",
-    creatorUserKey = "long user key as a string",
-    eventStartTime = 1234, // unix time
-    eventEndTime = 4321, // unix time
-    eventTypeEnum = 1 // not used for now
-}
-var database;
+// document.ready for jquery
+$(function() {
+
+    // Global Variables
+    var arrayOfAllEvents = [];
+    var exampleEvent = {
+        latitudeOfEvent = "lat",
+        longitudeOfEvent = "long",
+        eventHostName = "host",
+        eventName = "event",
+        nameOfEventLocation = "name",
+        addressOfEvent = "address",
+        creatorUserKey = "long user key as a string",
+        eventStartTime = 1234, // unix time
+        eventEndTime = 4321, // unix time
+        eventTypeEnum = 1 // not used for now
+    }
+    var database;
+
+    //event handler for create event
+    $("#submit").click(populateFireBaseWithEventInfo);
 
 
-// Database Helper Functions
-function pushUserKeyToLocalStorage(){
-}
+    // Database Helper Functions
+    function pushUserKeyToLocalStorage(){
+    }
 
-function testIfEventIsInProximityWindow(currentLocation, eventLocation, proximityWindowInMiles){
-    return true;
-}
+    function testIfEventIsInProximityWindow(currentLocation, eventLocation, proximityWindowInMiles){
+        return true;
+    }
 
-function returnArrayOfAllEventsWithinProximityWindow(currentLocation, proximityWindowInMiles){
-    return arrayOfProximateEvents;
-}
+    function returnArrayOfAllEventsWithinProximityWindow(currentLocation, proximityWindowInMiles){
+        return arrayOfProximateEvents;
+    }
 
-function populateTableWithEvents(){
-    var arrayOfProximateEvents = returnArrayOfAllEventsWithinProximityWindow(currentLocation, proximityWindowInMiles);
-    // jquery stuff to push events into repeating element
-}
+    function populateTableWithEvents(){
+        var arrayOfProximateEvents = returnArrayOfAllEventsWithinProximityWindow(currentLocation, proximityWindowInMiles);
+        // jquery stuff to push events into repeating element
+    }
 
-function populateFireBaseWithEventInfo(){
-    // jquery to populate info for event
-    // get all exampleEvent element from HTML
-    // push exampleEventObject to firebase
-}
+    function populateFireBaseWithEventInfo(){
+        // jquery to populate info for event
+        // get all exampleEvent element from HTML
+        // push exampleEventObject to firebase
+    }
 
-function connectToFirebase(){
-    var config = {
-        apiKey: "AIzaSyAboCNAM8LyGsG2dV-fzJBiZu4UVQhxgHk",
-        authDomain: "meetupschedule-ab2c2.firebaseapp.com",
-        databaseURL: "https://meetupschedule-ab2c2.firebaseio.com",
-        projectId: "meetupschedule-ab2c2",
-        storageBucket: "",
-        messagingSenderId: "799738528055"
-    };
-    firebase.initializeApp(config);
-    database = firebase.database();
+    function connectToFirebase(){
+        var config = {
+            apiKey: "AIzaSyAboCNAM8LyGsG2dV-fzJBiZu4UVQhxgHk",
+            authDomain: "meetupschedule-ab2c2.firebaseapp.com",
+            databaseURL: "https://meetupschedule-ab2c2.firebaseio.com",
+            projectId: "meetupschedule-ab2c2",
+            storageBucket: "",
+            messagingSenderId: "799738528055"
+        };
+        firebase.initializeApp(config);
+        database = firebase.database();
 
-}
+    }
+
+});
